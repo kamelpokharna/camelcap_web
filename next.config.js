7a6817bc4e35f303388f38d5a0b42ed8103ebc0a
kushsingh11 <61575121+kushsingh11@users.noreply.github.com>
2023-12-13 14:28:14 +0530

    Resolve issue 1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    images: {
+        unoptimized: true,
+        domains: ['camelcapital.vc'],
+        formats: ['image/avif', 'image/webp'],
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'camelcapital.vc',
+            port: '',
+            pathname: '**',
+        }, ]
+    }
 }
 
 module.exports = nextConfig
